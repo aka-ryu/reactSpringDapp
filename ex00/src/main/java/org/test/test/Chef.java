f73001d296b48a17a9361fcddeb58dc392390eee
DESKTOP-7CLU8AB\dev3_4 <uhas2003@naver.com>
2022-03-15 20:32:16 +0900

    s

---
+++ ex00/src/main/java/org/test/test/Chef.java
@@ -0,0 +1,12 @@
+package org.test.test;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Data;
+
+@Component
+@Data // lombok 으로 게터/세터/생성자를 코딩할 필요가없다.
+public class Chef {
+
+
+}
---
+++ ex00/src/main/java/org/test/test/SampleHotel.java
@@ -0,0 +1,19 @@
+package org.test.test;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.ToString;
+
+@Component
+@ToString
+@Getter
+public class SampleHotel {
+	
+	private Chef chef;
+	
+	public SampleHotel(Chef chef) {
+		this.chef = chef;
+	}
+
+}
